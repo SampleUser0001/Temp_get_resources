6aede0e2b3bfdc215a33c7eed830b526c4e82f0a
Satoru Tanaka <ittimfn@gmail.com>
2020-07-08 14:48:08 +0900

    psのログ取得を削除。(topだけで足りるはず。)

--- get_resources_log.sh
+++ get_resources_log.sh
@@ -1,19 +1,13 @@
 #!/bin/sh   
 
 FILE_DATE=`date +"%Y%m%d_%H%M%S"`
+DIR_DATE=`date +"%Y%m%d"`
 
 # topのログを配置する用のディレクトリが無かったら作成する。
-if [ ! -e ./top_logs ]; then 
-  mkdir ./top_logs
+if [ ! -e ./top_logs/${DIR_DATE} ]; then 
+  mkdir -p ./top_logs/${DIR_DATE}
 fi
 # topのログを取得する。
-top -b -n 1 > ./top_logs/top_${FILE_DATE}.txt
-
-# psのログを配置する用のディレクトリが無かったら作成する。
-if [ ! -e ./ps_logs ]; then 
-  mkdir ./ps_logs
-fi
-# psのログを取得する。
-ps aefux > ./ps_logs/ps_${FILE_DATE}.txt
+top -b -n 1 > ./top_logs/${DIR_DATE}/top_${FILE_DATE}.txt
 
 # oracle processのログの取得はRedmine参照。
